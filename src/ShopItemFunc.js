c0a627e2f7bd34b67f9dd72341b59245ac44095b
Андрей Увиков <uvikmedium@mail.ru>
2023-05-07 16:26:35 +0300

    first commit

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ShopItemFunc.css';
+
+class ShopItemFunc extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {        
+        return <>
+        <div class="main-content">
+            <h2>{this.props.item.brand}</h2>
+            <h1>{this.props.item.title}</h1>
+            <h3>{this.props.item.description}</h3>
+            <div class="description">{this.props.item.descriptionFull}</div>
+            <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+            <div class="divider"></div>
+            <div class="purchase-info">
+                <div class="price">{this.props.item.currency}{this.props.item.price.toFixed(2)}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    </>;
+    }
+}
+
+export default ShopItemFunc;
